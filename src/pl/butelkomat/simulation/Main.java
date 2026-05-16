1ecd83362331bba832ef938deb6208247d961ec8
sdmati <msiudy.ms@gmail.com>
2026-05-16 21:58:49 +0200

    poprawka bledu

--- src/pl/butelkomat/simulation/Main.java
+++ src/pl/butelkomat/simulation/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         WorldMap worldMap = new WorldMap(100, 100);
         Butelkomat butelkomat = new Butelkomat(100, 10, new Position(100, 100));
-        Zone zone = new Zone("CIpka",-1, 4, 122, 2312, 2.5);
+        Zone zone = new Zone("asdasd",-1, 4, 122, 2312, 2.5);
         worldMap.addButelkomat(butelkomat);
         worldMap.addZone(zone);
     }

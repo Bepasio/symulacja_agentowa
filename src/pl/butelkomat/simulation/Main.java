1a27275eae8fe8cfaa58121e0beec760f8f02beb
adamrzepecki <290526@student.pwr.edu.pl>
2026-05-17 18:27:10 +0200

    3zony

--- src/pl/butelkomat/simulation/Main.java
+++ src/pl/butelkomat/simulation/Main.java
@@ -10,9 +10,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-        WorldMap worldMap = new WorldMap(100, 100);
+        WorldMap worldMap = new WorldMap(89, 25);
         Butelkomat butelkomat = new Butelkomat(100, 10, new Position(100, 100));
-        Zone zone = new Zone("asdasd",-1, 4, 122, 2312, 2.5);
+        Zone zone = new Zone("Wyspa Słodowa",7, 18, 3, 7, 2.0);
+        Zone zone2 = new Zone("Ogród Botaniczny", 32, 63, 0, 6, 2.0);
+        Zone zone3 = new Zone("Plac Grunwaldzki", 54, 89, 7, 14, 1.5);
+        Zone zone4 = new Zone("Politechnika Wrocławska", 50, 89, 15, 25, 1.2);
         worldMap.addButelkomat(butelkomat);
         worldMap.addZone(zone);
     }

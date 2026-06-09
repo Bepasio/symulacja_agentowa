54f60413b501efbf9aa253e1998a4dc368351b04
sdmati <msiudy.ms@gmail.com>
2026-06-09 18:50:47 +0200

    wiecej komentarzy

--- core/src/main/java/pl/butelkomat/simulation/agents/Agent.java
+++ core/src/main/java/pl/butelkomat/simulation/agents/Agent.java
@@ -16,12 +16,12 @@ public abstract class Agent implements MapElement {
     protected Position position;
     protected int backpackCapacity;
     protected ArrayList<Bottle> bottles;
-    protected Position currentTarget;
+    protected Position currentTarget; //to jest okreslony cel podrozy, np butelkomat; przekazujemy z kazdym tickiem wywolujac moveTowards
     protected ArrayList<Position> visitedTargets;
     protected int id;
     protected double balance;
     protected List<Position> currentPath = null;
-    protected Position pathTarget = null;
+    protected Position pathTarget = null; //to jest cel ktory zapisujemy, zeby nie wyszukiwac za kazdym razem sciezki do celu; jesli to sie rozni od targetu przekazanego w moveTowards, to wtedy zmieniamy ten cel
 
     public Agent(Position startPosition, int backpackCapacity, int id) {
         this.position = startPosition;
@@ -42,26 +42,26 @@ public abstract class Agent implements MapElement {
     }
 
     public void moveTowards(Position target, WorldMap map){
-        if (target == null || position.equals(target)) return;
+        if (target == null || position.equals(target)) return; //jesli nie przekazano celu albo agent juz tam jest, to przerywamy
 
-        if(pathTarget == null || !pathTarget.equals(target)){
+        if(pathTarget == null || !pathTarget.equals(target)){ //jesli target zmienil sie od poprzedniego ticka, to czyscimy stary path i zapamietujemy nowy cel
             currentPath = null;
             pathTarget = target;
         };
 
         if(currentPath == null){
-            currentPath = map.pathFinder(position, target);
+            currentPath = map.pathFinder(position, target); //jesli nie ma sciezki, to obliczamy nową (1 cel - 1 sciezka)
         }
 
-        if(currentPath == null){
-            visitedTargets.add(target);
+        if(currentPath == null){ //jesli null to znaczy ze nie da sie dojsc
+            visitedTargets.add(target); //czarna lista bo punkt nieosioagalny
             currentTarget = null;
             pathTarget = null;
             return;
         }
 
-        if(!currentPath.isEmpty()){
-            Position nextStep = currentPath.remove(0);
+        if(!currentPath.isEmpty()){ //jesli w sciezce mamy punkty
+            Position nextStep = currentPath.remove(0); //pobieramy pierwszy z brzegu i ustawiamy na niego agenta (1 tick - 1 ruch)
             position.setX(nextStep.getX());
             position.setY(nextStep.getY());
         }
